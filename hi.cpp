b85d773f011ba61e482cbfb4a4a3541deed6ad4b
Michelle Davis <MDavis02@dwc.edu>
2016-01-18 20:28:47 -0500

    add all

--- hi.cpp
+++ hi.cpp
@@ -1,1 +1,21 @@
-Hi!
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	bool isprime = true;
+	{for (int n = 3; n <= 100; n++)
+		for (int d = 2; d < n; d++)
+			if (n % d == 0)
+			{
+				isprime = false;
+				break;
+			}
+
+	if (isprime == true)
+		cout << n << '\t';
+	}
+
+	return 0;
+
+}
